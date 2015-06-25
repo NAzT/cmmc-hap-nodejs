5198073455f4f2592aefe984f1d6df8003ae6488
Nat Weerawan <nat.wrw@gmail.com>
2015-06-25 18:19:36 +0700

    Add mqtt support.

--- accessories/Light_accessory.js
+++ accessories/Light_accessory.js
@@ -1,92 +1,157 @@
-var Accessory = require('../').Accessory;
-var Service = require('../').Service;
-var Characteristic = require('../').Characteristic;
-var uuid = require('../').uuid;
+// HomeKit types required
+var types = require("./types.js")
+var exports = module.exports = {};
+var mqtt    = require('mqtt');
+var client  = mqtt.connect('mqtt://128.199.104.122');
+ 
 
-// here's a fake hardware device that we'll expose to HomeKit
-var FAKE_LIGHT = {
-  powerOn: false,
-  brightness: 100, // percentage
-  
-  setPowerOn: function(on) { 
-    console.log("Turning the light %s!", on ? "on" : "off");
-    FAKE_LIGHT.powerOn = on;
-  },
-  setBrightness: function(brightness) {
-    console.log("Setting light brightness to %s", brightness);
-    FAKE_LIGHT.brightness = brightness;
-  },
-  identify: function() {
-    console.log("Identify the light!");
-  }
+var execute = function(accessory,characteristic,value){ 
+
+    client.on('connect', function () {
+
+    });
+ 
+// client.on('message', function (topic, message) {
+//   // message is Buffer 
+//   console.log(message.toString());
+//   client.end();
+// });
+
+
+
+    console.log("executed accessory: " + accessory + ", and characteristic: " + characteristic + ", with value: " +  value + "."); }
+
+exports.accessory = {
+  displayName: "Light 1",
+  username: "1A:2B:3C:4D:5E:FF",
+  pincode: "031-45-154",
+  services: [{
+    sType: types.ACCESSORY_INFORMATION_STYPE, 
+    characteristics: [{
+    	cType: types.NAME_CTYPE, 
+    	onUpdate: null,
+    	perms: ["pr"],
+		format: "string",
+		initialValue: "Light 1",
+		supportEvents: false,
+		supportBonjour: false,
+		manfDescription: "Bla",
+		designedMaxLength: 255    
+    },{
+    	cType: types.MANUFACTURER_CTYPE, 
+    	onUpdate: null,
+    	perms: ["pr"],
+		format: "string",
+		initialValue: "Oltica",
+		supportEvents: false,
+		supportBonjour: false,
+		manfDescription: "Bla",
+		designedMaxLength: 255    
+    },{
+    	cType: types.MODEL_CTYPE,
+    	onUpdate: null,
+    	perms: ["pr"],
+		format: "string",
+		initialValue: "Rev-1",
+		supportEvents: false,
+		supportBonjour: false,
+		manfDescription: "Bla",
+		designedMaxLength: 255    
+    },{
+    	cType: types.SERIAL_NUMBER_CTYPE, 
+    	onUpdate: null,
+    	perms: ["pr"],
+		format: "string",
+		initialValue: "A1S2NASF88EW",
+		supportEvents: false,
+		supportBonjour: false,
+		manfDescription: "Bla",
+		designedMaxLength: 255    
+    },{
+    	cType: types.IDENTIFY_CTYPE, 
+    	onUpdate: null,
+    	perms: ["pw"],
+		format: "bool",
+		initialValue: false,
+		supportEvents: false,
+		supportBonjour: false,
+		manfDescription: "Identify Accessory",
+		designedMaxLength: 1    
+    }]
+  },{
+    sType: types.LIGHTBULB_STYPE, 
+    characteristics: [{
+    	cType: types.NAME_CTYPE,
+    	onUpdate: null,
+    	perms: ["pr"],
+		format: "string",
+		initialValue: "Light 1 Light Service",
+		supportEvents: false,
+		supportBonjour: false,
+		manfDescription: "Bla",
+		designedMaxLength: 255   
+    },{
+    	cType: types.POWER_STATE_CTYPE,
+    	onUpdate: function(value) { 
+          if (value == true) {
+              client.publish('esp8266-18:fe:34:fe:81:e9', '0');
+              client.publish('esp8266-18:fe:34:fe:81:d3', '0');
+              client.publish('esp8266-18:fe:34:fe:c0:ff', '0');
+
+          }
+          else {
+              client.publish('esp8266-18:fe:34:fe:81:e9', '1');
+              client.publish('esp8266-18:fe:34:fe:81:d3', '1');
+              client.publish('esp8266-18:fe:34:fe:c0:ff', '1');
+          }            
+
+        },
+    	perms: ["pw","pr","ev"],
+		format: "bool",
+		initialValue: false,
+		supportEvents: false,
+		supportBonjour: false,
+		manfDescription: "Turn On the Light",
+		designedMaxLength: 1    
+    },{
+    	cType: types.HUE_CTYPE,
+    	onUpdate: function(value) { console.log("Change:",value); execute("Test Accessory 1", "Light - Hue", value); },
+    	perms: ["pw","pr","ev"],
+		format: "int",
+		initialValue: 0,
+		supportEvents: false,
+		supportBonjour: false,
+		manfDescription: "Adjust Hue of Light",
+		designedMinValue: 0,
+		designedMaxValue: 360,
+		designedMinStep: 1,
+		unit: "arcdegrees"
+    },{
+    	cType: types.BRIGHTNESS_CTYPE,
+    	onUpdate: function(value) { console.log("Change:",value); execute("Test Accessory 1", "Light - Brightness", value); },
+    	perms: ["pw","pr","ev"],
+		format: "int",
+		initialValue: 0,
+		supportEvents: false,
+		supportBonjour: false,
+		manfDescription: "Adjust Brightness of Light",
+		designedMinValue: 0,
+		designedMaxValue: 100,
+		designedMinStep: 1,
+		unit: "%"
+    },{
+    	cType: types.SATURATION_CTYPE,
+    	onUpdate: function(value) { console.log("Change:",value); execute("Test Accessory 1", "Light - Saturation", value); },
+    	perms: ["pw","pr","ev"],
+		format: "int",
+		initialValue: 0,
+		supportEvents: false,
+		supportBonjour: false,
+		manfDescription: "Adjust Saturation of Light",
+		designedMinValue: 0,
+		designedMaxValue: 100,
+		designedMinStep: 1,
+		unit: "%"
+    }]
+  }]
 }
-
-// Generate a consistent UUID for our light Accessory that will remain the same even when
-// restarting our server. We use the `uuid.generate` helper function to create a deterministic
-// UUID based on an arbitrary "namespace" and the word "light".
-var lightUUID = uuid.generate('hap-nodejs:accessories:light');
-
-// This is the Accessory that we'll return to HAP-NodeJS that represents our fake light.
-var light = exports.accessory = new Accessory('Light', lightUUID);
-
-// Add properties for publishing (in case we're using Core.js and not BridgedCore.js)
-light.username = "1A:2B:3C:4D:5E:FF";
-light.pincode = "031-45-154";
-
-// set some basic properties (these values are arbitrary and setting them is optional)
-light
-  .getService(Service.AccessoryInformation)
-  .setCharacteristic(Characteristic.Manufacturer, "Oltica")
-  .setCharacteristic(Characteristic.Model, "Rev-1")
-  .setCharacteristic(Characteristic.SerialNumber, "A1S2NASF88EW");
-
-// listen for the "identify" event for this Accessory
-light.on('identify', function(paired, callback) {
-  FAKE_LIGHT.identify();
-  callback(); // success
-});
-
-// Add the actual Lightbulb Service and listen for change events from iOS.
-// We can see the complete list of Services and Characteristics in `lib/gen/HomeKitTypes.js`
-light
-  .addService(Service.Lightbulb, "Fake Light") // services exposed to the user should have "names" like "Fake Light" for us
-  .getCharacteristic(Characteristic.On)
-  .on('set', function(value, callback) {
-    FAKE_LIGHT.setPowerOn(value);
-    callback(); // Our fake Light is synchronous - this value has been successfully set
-  });
-
-// We want to intercept requests for our current power state so we can query the hardware itself instead of
-// allowing HAP-NodeJS to return the cached Characteristic.value.
-light
-  .getService(Service.Lightbulb)
-  .getCharacteristic(Characteristic.On)
-  .on('get', function(callback) {
-    
-    // this event is emitted when you ask Siri directly whether your light is on or not. you might query
-    // the light hardware itself to find this out, then call the callback. But if you take longer than a
-    // few seconds to respond, Siri will give up.
-    
-    var err = null; // in case there were any problems
-    
-    if (FAKE_LIGHT.powerOn) {
-      console.log("Are we on? Yes.");
-      callback(err, true);
-    }
-    else {
-      console.log("Are we on? No.");
-      callback(err, false);
-    }
-  });
-
-// also add an "optional" Characteristic for Brightness
-light
-  .getService(Service.Lightbulb)
-  .addCharacteristic(Characteristic.Brightness)
-  .on('get', function(callback) {
-    callback(null, FAKE_LIGHT.brightness);
-  })
-  .on('set', function(value, callback) {
-    FAKE_LIGHT.setBrightness(value);
-    callback();
-  })
